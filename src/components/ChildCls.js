c9c911e2a46b162ee4803f8f17cca76810203d88
di2pro <dimmond.pro@yandex.ru>
2022-11-21 13:21:11 +0300

    remove useless constructors

--- src/components/ChildCls.js
+++ src/components/ChildCls.js
@@ -3,10 +3,6 @@ import { Component } from "react";
 import { randomHex } from "../util/randomHex";
 
 export default class ChildCls extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     const rndHex = randomHex();
     return <div className="Child" style={{ backgroundColor: rndHex }}>Class child component with {rndHex}</div>
